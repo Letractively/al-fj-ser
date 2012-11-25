7995e26f19aa9a0f4a2d980fdb9f98d4cb8b2654
Sergio.Molina.Guarneros@gmail.com <Sergio.Molina.Guarneros@gmail.com@52019c5e-7d4a-878f-7896-2ef43a1ecf35>
2012-11-25 22:49:56 +0000

    Resgistrar salida Completo Sergio

--- scacc-uatx/trunk/registrarSalida.aspx
+++ scacc-uatx/trunk/registrarSalida.aspx
@@ -1,11 +1,61 @@
-﻿<%@ Page Title="About Us" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+﻿<%@ Page Title="Registrar Salida" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
     CodeFile="registrarSalida.aspx.cs" Inherits="About" %>
 
 <asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+    <style type="text/css">
+        .style4
+        {
+            width: 100%;
+        }
+        .style5
+        {
+            width: 500px;
+        }
+        .style6
+        {
+            width: 108px;
+        }
+        .style7
+        {
+            width: 108px;
+            height: 29px;
+        }
+        .style8
+        {
+            width: 500px;
+            height: 29px;
+        }
+        .style9
+        {
+            height: 29px;
+        }
+    </style>
 </asp:Content>
 <asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
     <h2>
         Registrar Salida</h2>
-    <p>
-        &nbsp;</p>
+    <table class="style4">
+        <tr>
+            <td class="style7">
+                Matricula</td>
+            <td class="style8">
+                <asp:TextBox ID="tMatriculaSalida" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:Label ID="statusSalida" runat="server"></asp:Label>
+            </td>
+            <td class="style9">
+            </td>
+        </tr>
+        <tr>
+            <td class="style6">
+                &nbsp;</td>
+            <td class="style5">
+                <br />
+                <asp:Button ID="bRegistrarSalida" runat="server" 
+                    onclick="bRegistrarSalida_Click" Text="Regstrar Salida" />
+            </td>
+            <td>
+                &nbsp;</td>
+        </tr>
+    </table>
 </asp:Content>
--- scacc-uatx/trunk/registrarSalida.aspx.cs
+++ scacc-uatx/trunk/registrarSalida.aspx.cs
@@ -4,11 +4,83 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
 
 public partial class About : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+
+    }
+    protected void bRegistrarSalida_Click(object sender, EventArgs e)
+    {
+        Boolean rSalida = false;
+
+        if (tMatriculaSalida.Text == "")
+        {
+            statusSalida.Text = "Debe ingresar Matricula";
+        }
+        else {
+            String cons = "select hora_salida from computadora_apartada where matricula_alumno="+tMatriculaSalida.Text.Trim()+";";
+            try
+            {
+                MySqlConnection Conexion = new MySqlConnection();
+                String cadena;
+                cadena = "Server=localhost; user=root; database=Laboratorio";
+                Conexion.ConnectionString = cadena;
+                Conexion.Open();
+
+                MySqlCommand command = new MySqlCommand(cons, Conexion);
+                MySqlDataReader resultado = command.ExecuteReader();
+
+                if (resultado.Read())
+                {
+                    rSalida = true;
+                }
+                else
+                    rSalida = false;
+                resultado.Close();
+                Conexion.Close();
+            }
+
+            catch (MySqlException ex)
+            {
+                rSalida = false;
+            }
+
+
+            if (rSalida) 
+            {
+
+                String hora = "'" + DateTime.Now.Hour.ToString() + ":" + DateTime.Now.Minute.ToString() + ":" + DateTime.Now.Second.ToString() + "'";
+           
+                String consulta = "UPDATE computadora_apartada SET hora_salida = "+hora+" WHERE matricula_alumno ="+tMatriculaSalida.Text+" and hora_salida='00:00:00'";
+               //statusSalida.Text = consulta;
+                
+                try
+                {
+                    MySqlConnection Conexion = new MySqlConnection();
+                    String cadena;
+                    cadena = "Server=localhost; user=root; database=Laboratorio";
+                    Conexion.ConnectionString = cadena;
+                    Conexion.Open();
+                    MySqlCommand command = new MySqlCommand(consulta, Conexion);
+                    command.ExecuteNonQuery();
+                    Conexion.Close();
+
+                    statusSalida.Text = "Se ha registrado satisfactoriamente tu Salida ";
+                    
+                }
+                catch (MySqlException ex)
+                {
+                    //Response.Write("<script language='javascript'>alert('Verifica tus Datos')</script>");
+                    statusSalida.Text = "No se puede realizar operacion, Verifica tus Datos";
+                }
+            }
+            
+            
+        }
 
     }
 }
